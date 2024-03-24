bf0b8ca6cacedfd82edb216f014a7e9fd4f35cd4
Rajul Thakur <thakurajul8888@gmail.com>
2024-03-24 10:36:27 +0530

    add: added users related api endpoints.

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,22 @@
+const User = require('../models/user.js').User;
+
+// Getting all users 
+const getUsers = async (req, res) => {
+    let success = true;
+    try {
+        const users = await User.find().select("-password")
+
+        return res.status(201).json({
+            success,
+            users
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+};
+
+module.exports = {  getUsers };
+
--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,14 @@ const connect = require('./utils/db.js')();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const userRouter = require('./routes/user.route');
 const orderRouter = require('./routes/order.route')
-const bodyParser = require('body-parser')
-const { Order } = require("./models/order")
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/orders', orderRouter);
-
+app.use('/user', userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`application is running on port ${process.env.PORT}`);
